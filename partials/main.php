69d65b5af44488eaf35a218e0b4bcfd6bd688624
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-05-05 15:23:42 +0200

    first milestone done

--- partials/main.php
+++ partials/main.php
@@ -1,10 +1,19 @@
 
 <main class="container-fluid">
-    <div id="card-container" class="container">
+    <div id="card-container" class="container justify-content-between d-flex flex-wrap gap-3">
         <?php 
-        for($i = 0; $i < count($albums); $i++){
+        foreach($albums as $album){
         ?>
-        <div>ciao</div>
+        <div class="text-white d-flex flex-column align-items-center col-2 ggf-card p-2">
+            <div>
+                <img class="w-100" src="<?= $album['poster']?>" alt="poster">
+            </div>
+            <h3 class="text-center"><?= $album['title']?></h3>
+            <div class="d-flex flex-column align-items-center">
+                <span><?= $album['author']?></span>
+                <span><?= $album['year']?></span>
+            </div>
+        </div>
         <?php    
         }
         ?>
